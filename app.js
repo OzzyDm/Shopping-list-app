d6e47964cfc9d34eef40326ebf9346dd9f9cdd8e
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-23 20:47:20 -0400

    adding item updated

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const addButton = document.querySelector("#add");
-const enterItem = document.querySelector("#enterItem");
-
-newItem.innerText = enterItem.value;
 
 function addItem() {
+  const enterItem = document.querySelector("#enterItem").value;
   const ul = document.querySelector("#list");
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode("blabla"));
+  li.appendChild(document.createTextNode(enterItem));
   ul.appendChild(li);
 }
 
 addButton.addEventListener("click", addItem);
+
+const textbox = document.getElementById("enterItem");
+textbox.addEventListener("keypress", function onEvent(event) {
+  if (event.key === "Enter") {
+    document.getElementById("add").click();
+    clearTextarea();
+  }
+});
+
+function clearTextarea() {
+  document.querySelector("#enterItem").value = "";
+}
